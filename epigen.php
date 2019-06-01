85c53b07466fd646de5423fd75adec597eae69db
florianst <florian.stortz@cs.ox.ac.uk>
2019-06-01 12:21:21 +0100

    get number of matching/annotated targets

--- epigen.php
+++ epigen.php
@@ -5,34 +5,49 @@ include "inc/header.php";
 <h2>Epigenetics Studies</h2>
 <p>These are the epigenetics studies which are currently included in the database.</p>
 
-<table class="table table-striped">
+
 <?php
-$result = $conn->query("SELECT id, cell_line, assay, local_path FROM epigenetics_experiments");
+$result = $conn->query("SELECT id, cell_line, assay, genome, COUNT(id) AS count FROM epigenetics_experiments WHERE file_format = 0 GROUP BY cell_line, assay"); // group the data according to assay type and cell line
 if ($result->num_rows > 0) {
-    echo '<thead class="thead-dark">
+    echo '<table class="table table-striped">
+          <thead class="thead-dark">
           <tr>
             <th scope="col">No.</th>
             <th scope="col">study</th>
             <th scope="col">assay type</th>
             <th scope="col">cell line</th>
+            <th scope="col">total matching targets</th>
             <th scope="col">annotated targets</th>
           </tr>
           </thead>
           <tbody>';
     
-    // output data of each row
-    $i = 0;
-    while($row = $result->fetch_assoc()) {
+    while($row = $result->fetch_assoc()) { // output data of each row
         $i++;
         $studyname = end(explode('/', $row["local_path"]));
         $studyname = explode('.', $studyname)[0]; // gets rid of file ending
         $studyname = explode('_', $studyname)[0]; // gets rid of assay category (if included)
-        $result2 = $conn->query("SELECT id FROM cleavage_data WHERE epigenetics_ids LIKE '%{$studyname}%'");
-        echo '<tr><th scope="row">'.$i.'</th><td>'.$studyname.'</td><td>'.$row["assay"].'</td><td>'.$row["cell_line"].'</td><td>'.$result2->num_rows.'</td></tr>';
+        $assay = strtolower($row["assay"]);
+        $result2 = $conn->query("SELECT id FROM cleavage_data WHERE cell_line='{$row["cell_line"]}' AND epigen_{$assay} != ''");
+        $result3 = $conn->query("SELECT id FROM cleavage_data WHERE epigenetics_ids LIKE '%EH%' AND cell_line='{$row["cell_line"]}' AND epigen_{$assay} != ''");
+        echo '<tr><th scope="row">'.$i.'</th><td>SCREEN ENCODE v4</td><td>'.$row["assay"].'</td><td>'.$row["cell_line"].'</td><td>'.$result2->num_rows.'</td><td>'.$result3->num_rows.'</td></tr>';
     }
-    
-    echo "</tbody>";
 }
+    
+
+$result = $conn->query("SELECT id, cell_line, assay, local_path FROM epigenetics_experiments WHERE file_format != 0");  
+while($row = $result->fetch_assoc()) { // output data of each row
+    $i++;
+    $studyname = end(explode('/', $row["local_path"]));
+    $studyname = explode('.', $studyname)[0]; // gets rid of file ending
+    $studyname = explode('_', $studyname)[0]; // gets rid of assay category (if included)
+    $result2 = $conn->query("SELECT id FROM cleavage_data WHERE cell_line='{$row["cell_line"]}'");
+    $result3 = $conn->query("SELECT id FROM cleavage_data WHERE epigenetics_ids LIKE '%{$studyname}%'");
+    echo '<tr><th scope="row">'.$i.'</th><td>'.$studyname.'</td><td>'.$row["assay"].'</td><td>'.$row["cell_line"].'</td><td>'.$result2->num_rows.'</td><td>'.$result3->num_rows.'</td></tr>';
+}
+
+echo "</tbody>";
+
 ?>
 </table>
 
