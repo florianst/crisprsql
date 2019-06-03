07ad177560ce3428661e09c2af4ae90d91106ed6
florianst <florian.stortz@cs.ox.ac.uk>
2019-06-03 19:16:25 +0100

    added download and studies pages

---
+++ download.php
@@ -0,0 +1,10 @@
+<?php 
+include "inc/header.php";
+?>
+<h2>Download</h2>
+<p>The benchmark dataset can be downloaded here.</p>
+
+<?php
+include "inc/footer.php";
+
+?>
---
+++ studies.php
@@ -0,0 +1,10 @@
+<?php 
+include "inc/header.php";
+?>
+<h2>Studies</h2>
+<p>The following studies are included in crisprSQL:</p>
+
+<?php
+include "inc/footer.php";
+
+?>
