8882f55572dd39aaa6fbf3fee26c62d3c4e8343a
florianst <florian.stortz@cs.ox.ac.uk>
2019-06-03 17:05:23 +0100

    add contact information

--- contact.php
+++ contact.php
@@ -1,6 +1,18 @@
 <?php 
 include "inc/header.php";
+?>
+<h2>Contact</h2>
+<p>crisprSQL is developed by <a href="http://users.ox.ac.uk/~lina3430" target="_blank">Florian Stortz</a> at Oxford University.</p>
+<br>
+<p>Department of Computer Science<br>
+University of Oxford<br>
+Wolfson Building<br>
+Parks Road<br>
+OXFORD<br>
+OX1 3QD</p>
 
+
+<?php
 
 include "inc/footer.php";
 
