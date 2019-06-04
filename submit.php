fe3f015ab397a890cb864722afce4588c16c8a41
florianst <florian.stortz@cs.ox.ac.uk>
2019-06-04 18:18:49 +0100

    implement file submission

--- submit.php
+++ submit.php
@@ -2,7 +2,32 @@
 include "inc/header.php";
 ?>
 <h2>Submit</h2>
-<p>We invite submissions of comma separated text files containing the following columns:</p>
+<?php 
+// check upload
+if (isset($_FILES['file'])) {
+    if($_FILES['file']['size'] > 10485760) { //10 MB (size is also in bytes)
+        // File too big
+        echo '<div class="alert alert-danger" role="alert">The uploaded file was too big.</div>';
+    } else {
+        // check if file is csv
+        $mimes = array('application/vnd.ms-excel','text/plain','text/csv','text/tsv');
+        if(in_array($_FILES['file']['type'], $mimes)) {
+            $target_file = $uploadpath.'/'.basename($_FILES["file"]["name"]);
+            if (file_exists($target_file)) {
+                echo '<div class="alert alert-danger" role="alert">Please choose a different file name.</div>';
+            } elseif (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+                echo "<div class='alert alert-success' role='alert'>Your file has been uploaded.</div>";
+            } else {
+                echo '<div class="alert alert-danger" role="alert">There was an error uploading your file. Please try again.</div>';
+            }
+        } else {
+            echo '<div class="alert alert-danger" role="alert">The uploaded file does not have the right format.</div>';
+        }
+    }
+}
+
+?>
+<p>We invite submissions of comma separated text files containing the following columns, to be included in the crisprSQL database:</p>
 <table border=0 class="rounded-list">
     <tr>
     <td>
@@ -19,19 +44,18 @@ include "inc/header.php";
     </td>
     </tr>
 </table>
-<p>Please also supply metadata about your publication.</p>
+<p>Please also supply metadata about your publication. The maximum file size is 10 MB.</p>
 
 <h3>Upload csv file</h3>
-<form>
+<form action="submit.php" method="post" enctype="multipart/form-data">
   <div class="custom-file">
-    <input type="file" class="custom-file-input" id="file">
+    <input type="file" class="custom-file-input" id="file" name="file">
     <label class="custom-file-label" for="file">Choose file</label>
   </div>
-  <div class="form-group">
-    <label for="additional">File metadata</label>
-    <textarea class="form-control" id="additional" rows="3" placeholder="PubMed-ID, publication link, measurement protocol, etc."></textarea>
+  <div class="form-group" style="padding-top:1em;">
+    <textarea class="form-control" id="additional" rows="3" placeholder="metadata: PubMed-ID, publication link, measurement protocol, contact email address, etc."></textarea>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary" name="submit">Submit</button>
 </form>
 
 <script>
