e511c1593dbeca3109af5efa924275e50a93a2bb
florianst <florian.stortz@cs.ox.ac.uk>
2019-06-04 15:06:29 +0100

    obfuscate email address

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 include "inc/header.php";
 ?>
 <h2>Contact</h2>
-<p>crisprSQL is developed by <a href="http://users.ox.ac.uk/~lina3430" target="_blank">Florian Stortz</a> at Oxford University.</p>
+<p>crisprSQL is developed by <a href="http://users.ox.ac.uk/~lina3430" target="_blank">Florian Stortz</a> at Oxford University. Please <A HREF="&#109;&#97;&#105;&#108;&#116;&#111;&#58;%66%6C%6F%72%69%61%6E%2E%73%74%6F%72%74%7A%40%63%73%2E%6F%78%2E%61%63%2E%75%6B">send an email</A> with any suggestions or comments.</p>
 <br>
 <p>Department of Computer Science<br>
 University of Oxford<br>
