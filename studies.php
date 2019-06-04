8222a463aa67fee97f8a9b70d9633cb0d8c03069
florianst <florian.stortz@cs.ox.ac.uk>
2019-06-04 15:33:34 +0100

    implement studies table

--- studies.php
+++ studies.php
@@ -5,6 +5,28 @@ include "inc/header.php";
 <p>The following studies are included in crisprSQL:</p>
 
 <?php
+$result = $conn->query("SELECT experiment_id, cell_line, COUNT(*) AS count FROM cleavage_data GROUP BY experiment_id, cell_line"); // group the data according to assay type and cell line
+if ($result->num_rows > 0) {
+    echo '<table class="table table-striped">
+          <thead class="thead-dark">
+          <tr>
+            <th scope="col">No.</th>
+            <th scope="col">study</th>
+            <th scope="col">cell line</th>
+            <th scope="col">total targets</th>
+            <th scope="col">epigenetically annotated targets</th>
+          </tr>
+          </thead>
+          <tbody>';
+    
+    while($row = $result->fetch_assoc()) { // output data of each row
+        $i++;
+        $result2 = $conn->query("SELECT id FROM cleavage_data WHERE experiment_id='{$row["experiment_id"]}' AND epigenetics_ids != ''");
+        echo '<tr><th scope="row">'.$i.'</th><td><a href="http://screen.encodeproject.org/" target="_blank">SCREEN ENCODE v4</a></td><td>'.$row["cell_line"].'</td><td>'.$row["count"].'</td><td>'.$result2->num_rows.'</td></tr>';
+    }
+    echo "</tbody></table>";
+}
+
 include "inc/footer.php";
 
 ?>
