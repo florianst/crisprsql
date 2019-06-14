78adb125bb7cba987ff55896429e3607a98ea437
florianst <florian.stortz@cs.ox.ac.uk>
2019-06-14 15:35:29 +0100

    plot guide position last, bugfix

--- inc/plot_offtargetprofile.php
+++ inc/plot_offtargetprofile.php
@@ -2,6 +2,7 @@
 // plot the offtarget profile of a given guide, handed over in an associative array with indices target_chr, target_start, target_end, cleavage_freq
 
 $chromLengths = array("chr1"=>224999719, "chr2"=>237712649, "chr3"=>194704827, "chr4"=>187297063, "chr5"=>177702766, "chr6"=>167273993, "chr7"=>154952424, "chr8"=>142612826, "chr9"=>120312298, "chr10"=>131624737, "chr11"=>131130853, "chr12"=>130303534, "chr13"=>95559980, "chr14"=>88290585, "chr15"=>81341915, "chr16"=>78884754, "chr17"=>77800220, "chr18"=>74656155, "chr19"=>55785651, "chr20"=>59505254, "chr21"=>34171998, "chr22"=>34893953, "chrX"=>151058754, "chrY"=>25121652);
+$totalLength = array_sum($chromLengths);
 
 function drawBorder(&$img, &$color, $thickness=1) {
     imagesetthickness($img, 1);
@@ -15,6 +16,17 @@ function drawBorder(&$img, &$color, $thickness=1) {
     }   
 }
 
+function getXcoord($chr, $start, $imgwidth) {
+    $chromLengths = $GLOBALS['chromLengths']; // get global variables
+    $totalLength  = $GLOBALS['totalLength'];
+    $index = array_search($chr, array_keys($chromLengths));
+    if ($index >= 0 && is_numeric($start)) {
+        $LengthToChr = array_sum(array_chunk($chromLengths, $index+1, TRUE)[0])-$chromLengths["chr1"];
+        $xpos = $imgwidth*($LengthToChr+$start)/$totalLength;
+        return ceil($xpos);
+    } else { return FALSE; }
+}
+
 function plotOfftargetProfile($array, $imgwidth=180, $imgheight=30) {
     // generate image
     $image = imagecreate($imgwidth, $imgheight);
@@ -25,22 +37,21 @@ function plotOfftargetProfile($array, $imgwidth=180, $imgheight=30) {
     $border_colour = imagecolorallocate($image, 0, 0, 0);       // black
     
     // draw vertical line for each target
-    $chromLengths = $GLOBALS['chromLengths']; // get global variable
     foreach ($array as $target) {
         // calculate x position of line from chromosome and start
-        $totalLength = array_sum($chromLengths);
-        $index = array_search($target["target_chr"], array_keys($chromLengths));
-        if ($index > 0 && is_numeric($target["target_start"])) {
-            $LengthToChr = array_sum(array_chunk($chromLengths, $index+1, TRUE)[0]);
-            $xpos = $imgwidth*($LengthToChr+$target["target_start"])/$totalLength;
+        $xpos = getXcoord($target["target_chr"], $target["target_start"], $imgwidth);
+        if ($xpos >= 0) {
             imagesetthickness($image, 2);
-            if ($target["grna_target_id"] == $target["id"]) { $guidexpos = $xpos; }
-            elseif ($target["cleavage_freq"] > 0.1) { $colour = $target_colour; }
-            else { imagesetthickness($image, 1); $colour = $weak_colour; }
+            if ($target["cleavage_freq"] > 0.1) { $colour = $target_colour; }
+            else { imagesetthickness($image, 1);  $colour = $weak_colour; }
             imageline($image, $xpos, 0, $xpos, $imgheight, $colour);
         }
     }
-    if (isset($guidexpos)) { imagesetthickness($image, 3); imageline($image, $guidexpos, 0, $guidexpos, $imgheight, $guide_colour); }
+    // draw line for the guide
+    imagesetthickness($image, 3);
+    $guidexpos = getXcoord($target["grna_target_chr"], $target["grna_target_start"], $imgwidth);
+    imageline($image, $guidexpos, 0, $guidexpos, $imgheight, $guide_colour);
+    
     drawBorder($image, $border_colour);
     
     // return as data URI
