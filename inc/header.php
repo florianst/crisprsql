375af0cc77a1a9c0ca2b1e82664ef7d8dbd4a88b
florianst <florian.stortz@cs.ox.ac.uk>
2019-06-18 15:04:29 +0100

    avoid phone number linking on iOS

--- inc/header.php
+++ inc/header.php
@@ -10,6 +10,7 @@ $navpages = array("index.php"=>"Home", "studies.php"=>"Studies", "search.php"=>"
   <title>crisprSQL - CRISPR off-target database</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="format-detection" content="telephone=no">
   <link rel="stylesheet" href="/css/bootstrap.min.css">
   <link rel="stylesheet" href="/css/design.css">
   <link rel="stylesheet" href="/css/bootstrap-sortable.css">
