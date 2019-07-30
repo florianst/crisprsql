56a7b03045d98097d3d56ae57d2281f02dbd8ccf
florianst <florian.stortz@cs.ox.ac.uk>
2019-07-30 12:47:47 +0100

    only show unique epigenetics identifiers

--- search.php
+++ search.php
@@ -69,7 +69,7 @@ if (isset($result)) {
             }
             $epigen_str = '';
             if (strlen($row["epigenetics_ids"]) > 0) {
-                $epigen_studies = explode(',', $row["epigenetics_ids"]);
+                $epigen_studies = array_unique(explode(',', $row["epigenetics_ids"]));
                 foreach ($epigen_studies as $study_identifier) {
                     if (strlen($study_identifier) > 2) { $epigen_str .= '<a href="https://www.encodeproject.org/search/?searchTerm='.$study_identifier.'" target="_new">'.$study_identifier.'</a>, '; }
                 }
