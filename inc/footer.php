952f33fa9cdcc4269baf1f275a95da8089938c54
Florian Stortz <florian.stoertz@gmail.com>
2020-05-18 21:12:44 +0200

    adjust date

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
 </div>
 <div class="jumbotron text-center" style="margin-bottom:0">
-  <p>crisprSQL is developed at Oxford University, 2019</p>
+  <p>crisprSQL is developed at Oxford University, 2020</p>
 </div>
 
 </body>
