65933b4851c609b24ae2817d47ba5c13b52798e8
Florian Stortz <florian.stoertz@gmail.com>
2020-06-29 19:58:35 +0100

    add analytics

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# crisprsql
-PHP online database for gene editing off-target studies
+# crisprSQL
+PHP online database for gene editing off-target studies.
+
+## Analytics
+crisprSQL uses the open-source web analytics tool [Matomo](https://matomo.org/). To update the geolocation database download "IP to City Lite" from https://db-ip.com/db/download/ip-to-city-lite, rename to DBIP-City.mmdb and upload to matomo/misc
--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 include "inc/header.php";
 ?>
 <h2>Contact</h2>
-<p>crisprSQL is developed by <a href="http://users.ox.ac.uk/~lina3430" target="_blank">Florian Stortz</a> at Oxford University. Please <A HREF="&#109;&#97;&#105;&#108;&#116;&#111;&#58;%66%6C%6F%72%69%61%6E%2E%73%74%6F%72%74%7A%40%63%73%2E%6F%78%2E%61%63%2E%75%6B">send an email</A> with any suggestions or comments.</p>
+<p>crisprSQL is developed by <a href="http://users.ox.ac.uk/~lina3430" target="_blank">Florian St&ouml;rtz</a> at Oxford University. Please <A HREF="&#109;&#97;&#105;&#108;&#116;&#111;&#58;%66%6C%6F%72%69%61%6E%2E%73%74%6F%72%74%7A%40%63%73%2E%6F%78%2E%61%63%2E%75%6B">send an email</A> with any suggestions or comments.</p>
 <br>
 <p>Department of Computer Science<br>
 University of Oxford<br>
@@ -10,6 +10,12 @@ Wolfson Building<br>
 Parks Road<br>
 OXFORD<br>
 OX1 3QD</p>
+
+<br>
+<!-- Matomo opt-out -->
+<h4>Analytics opt-out</h4>
+<iframe style="border: 0; height: 200px; width: 600px;" src="http://analytics.crisprsql.bplaced.net/index.php?module=CoreAdminHome&action=optOut&language=en&backgroundColor=&fontColor=&fontSize=&fontFamily='Segoe UI'"></iframe>
+
 
 
 <?php
