356ce1987215d5fbf867be943d45723881d748a0
Florian Stortz <florian.stoertz@gmail.com>
2020-07-02 15:17:00 +0100

    add privacy policy, disable tracking cookies

--- contact.php
+++ contact.php
@@ -12,10 +12,53 @@ OXFORD<br>
 OX1 3QD</p>
 
 <br>
+<h4>Privacy policy</h4>
+<p><small>We use the open source web analytics platform <a href="https://matomo.org/">Matomo</a> to measure, collect, analyse and report visitors data for purposes of understanding and optimizing our website. 
+If you would like to see what Matomo looks like, you can access a demo version at: <a href="https://demo.matomo.org">demo.matomo.org</a>.
+Matomo is used to analyse the behaviour of the website visitors to identify potential pitfalls; not found pages, search engine indexing issues, which contents are the most appreciated, etc. 
+Matomo is processing the following personal data:
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <ul>
+        <li>IP address with last two bits anonymised</li>
+        <li>Date and time, time in local timezone</li>
+        <li>Title and URL of the page being viewed</li>
+        <li>URL of the page that was viewed prior to the current page</li>
+        <li>Screen resolution</li>
+        <li>Files that were clicked and downloaded</li>
+      </ul>
+    </div>
+    <div class="col-sm">
+      <ul>
+        <li>Link clicks to an outside domain</li>
+        <li>Pages generation time</li>
+        <li>Rough country, region, city of the user</li>
+        <li>Main Language of the browser</li>
+        <li>User Agent of the browser</li>
+        <li>Form interactions such as searches</li>
+        </ul>
+    </div>
+  </div>
+</div>
+The processing of personal data with Matomo is based on legitimate interests. Processing your personal data such as cookies is helping us identify what is working and what is not on our website. 
+For example, it helps us identify if the way we are communicating is engaging or not and how we can organize the structure of the website better. By processing your personal data, you can profit 
+from a website which is getting better and better.
+Without the data, we would not be able to provide you the service we are currently offering to you. Your data will be used only to improve the user experience on our website and help you find the 
+information you are looking for.
+</small></p>
+<p><small>The personal data received through Matomo are stored on our web hosting server only. Matomo data is hosted in Germany. We are keeping the personal data captured within Matomo for a period of no more than 13 months.
+If you think that the way we process your personal data with Matomo analytics is infringing the law, you have the right to lodge a complaint with a supervisory authority. 
+If you wish us to not process any personal data with Matomo, you can opt-out from it at any time. There will be no consequences at all regarding the use of our website. Matomo is not doing any profiling.</small></p>
+<p><small>As Matomo is processing personal data on legitimate interests, you can exercise the following rights:
+<ul>
+<li>Right of access: you can ask us at any time to access your personal data.</li>
+<li>Right to erasure: you can ask us at any time to delete all the personal data we are processing about you.</li>
+<li>Right to object: you can object to the tracking of your personal data by using the following opt-out feature:
 <!-- Matomo opt-out -->
-<h4>Analytics opt-out</h4>
-<iframe style="border: 0; height: 200px; width: 600px;" src="http://analytics.crisprsql.bplaced.net/index.php?module=CoreAdminHome&action=optOut&language=en&backgroundColor=&fontColor=&fontSize=&fontFamily='Segoe UI'"></iframe>
-
+<iframe style="border: 0; height: 200px; width: 100%;" src="http://analytics.crisprsql.com/index.php?module=CoreAdminHome&action=optOut&language=en&backgroundColor=&fontColor=&fontSize=&fontFamily=-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,sans-serif"></iframe>
+</li></ul>
+</small></p>
 
 
 <?php
