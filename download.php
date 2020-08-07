682a77e4e397ea88959a0dc01fee5cf0e57d7922
Florian Stortz <florian.stoertz@gmail.com>
2020-08-07 18:57:46 +0200

    add 100720 version

--- download.php
+++ download.php
@@ -6,7 +6,10 @@ include "inc/header.php";
 <p>Various versions of the database can be downloaded here.</p>
 
 <div class="alert alert-primary half-length" role="alert">
-    <a href="downloads/260520.zip"><button type="button" class="btn btn-primary btn-lg">Download</button></a>   Version 26/05/2020
+    <a href="downloads/100720.zip"><button type="button" class="btn btn-primary btn-lg">Download</button></a>   Version 10/07/2020
+</div>
+<div class="alert alert-secondary half-length" role="alert">
+    <a href="downloads/260520.zip"><button type="button" class="btn btn-secondary btn-lg">Download</button></a> Version 26/05/2020
 </div>
 <div class="alert alert-secondary half-length" role="alert">
     <a href="downloads/140520.zip"><button type="button" class="btn btn-secondary btn-lg">Download</button></a> Version 14/05/2020
